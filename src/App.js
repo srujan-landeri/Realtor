6db6f992fd2f173f580aa9577d4715df8023ffde
Landeri Srujan <srujanlanderi@gmail.com>
2023-10-22 20:00:40 +0530

    Added Routes and Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+/* Importing styles */
 import './App.css';
 
+/* Importing Routing components */
+import {BrowserRouter, Switch, Routes, Route} from 'react-router-dom';
+
+/* Importing pages */
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Profile from './pages/Profile'
+import Offers from './pages/Offers'
 function App() {
+
   return (
     <>
-      <h1 className="">Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/offers" element={<Offers/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <div>
+      <h1>Fogot Password</h1>
+    </div>
+  )
+}
